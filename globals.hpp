3dba2e0f118e61560b1503d28770405aecfdaeb3
jonathanserb <jonathanserb@gmail.com>
2025-04-15 20:50:05 -0400

    Changed servo ports to standard GPIO ports

--- globals.hpp
+++ globals.hpp
@@ -1,22 +1,17 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-/*
- * Not sure where these PWM pins are used (maybe speaker?),
- * left them there just in case, but need to use two separate
- * PWM signals for the servos.
- */
 #define LEFT_PWM 12
 
-#define TUMBLER_SERVO 12
-
 #define RIGHT_PWM 13
-
-#define PENDULUM_SERVO 13
 
 #define SWINGOVER_ARM_BTN 17
 
 #define ZIPLINE_BTN 22
+
+#define TUMBLER_SERVO 23
+
+#define PENDULUM_SERVO 24
 
 #define PROX_SENSOR 27
 
